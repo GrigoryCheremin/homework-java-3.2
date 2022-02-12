607c6bf83374e683b18afeedf8ec43c1b44d7059
GrigoryCheremin <Greenx.git.work@gmail.com>
2022-02-12 08:05:11 +0300

    revision after review

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,5 @@
 public class BmiService {
-    public int calculate() {
-        int growth = 177; // Рост в сантиметрах
-        int bodyMass = 85; // Масса тела в килограммах
+    public int calculate(int growth, int bodyMass) {
         int bmi = bodyMass * 10000 / (growth * growth); // 10 000 используется для перевода роста в метры
         return bmi;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int bmi = service.calculate();
+        int growth = 177; // Рост в сантиметрах
+        int bodyMass = 85; // Масса тела в килограммах
+        int bmi = service.calculate(growth, bodyMass);
         System.out.println ("Индекс массы тела составляет: " + bmi);
     }
 }
